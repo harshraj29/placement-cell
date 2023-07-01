e6a6d2b51fa92aa415beffe4d7b7c1b6f86d1e27
harshraj29 <rajh3282@gmail.com>
2023-07-01 15:39:07 +0530

    fix bug

--- index.js
+++ index.js
@@ -4,74 +4,56 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 9000;
 
-
 const expressLayouts = require('express-ejs-layouts');
 
-//Used for session cookie
+// Used for session cookie
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const session = require("express-session");
 
-
 const db = require('./config/mongoose');
 const path = require('path')
 
-
-
-
-
+// Parse URL-encoded data
 app.use(express.urlencoded());
 
-
 app.use(cookieParser());
+
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
 
 app.use(expressLayouts);
 app.use(express.static('./assets'));
 
-//Use Express routers
+// Use Express routers
 app.use('/', require('./routes'));
 
-//Set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-// mongo store is used to store the session cookie in the db
+// Configure session management
 app.use(
   session({
-    name: "authentication",
-    // TODO change the secret before deployment in production mode
-    // secret: env.session_cookies_key,
-    secret: 'blahsmmm',
+    name: "authentication", // Name of the session cookie
+    secret: 'blahsmmm', // Secret used to sign the session ID cookie
     saveUninitialized: false,
     resave: false,
     cookie: {
-      maxAge: 1000 * 60 * 100,
+      maxAge: 1000 * 60 * 100, // Cookie expiration time (in milliseconds)
     },
-  //   store: new MongoStore(
-  //     {
-  //       mongooseConnection: 'mongodb://127.0.0.1:27017/PlacementCell',
-  //       autoRemove: "disabled",
-  //     },
-  //     function (err) {
-  //       console.log(err || "connect-mongodb setup ok");
-  //     }
-  //   ),
   })
 );
+
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Middleware to set the authenticated user in the request object
 app.use(passport.setAuthenticatedUser);
 
-// app.get('/', function(req,res){
-//   res.send('<h1>Cool Go</h1>');
-// })
+// Start the server and listen on the specified port
 app.listen(port, function (err) {
     if (err) {
       console.log(`Error in running the server: ${err}`);
     }
-    // console.log("gidbuhsbvufdhbvuffbsevyue", process.env);
     console.log(`Server is running on port: ${port}`);
-  });
+});
